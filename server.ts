587bc416c7dc6b240ed8a44ee13cda2ecd857bfe
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2017-02-07 21:13:46 +0200

    ад

--- server.ts
+++ server.ts
@@ -1,17 +1,4 @@
-var express = require ('express');
-var Server = require ('socket.io');
-var socket = new Server ();
-var app = express ();
-app.use(express.static(__dirname))
-
-let number_of_videos = 10;
-let comments = [];
-
-for (let i = 0 ; i < number_of_videos ; i += 1)
-{
-	comments.push ([]);
-}
-
+/*
 socket.on ('connect', function () {
 	console.log ("new connection");
 
@@ -31,21 +18,50 @@ socket.on ('connect', function () {
 		console.log ("new disconnection");
 	});
 });
+*/
 
-app.get ('/data', function (req, res) {
-	let id = parseInt (req.query.id);
-	console.log (req.query.id);
-	res.send (JSON.stringify (comments [id]));
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var fs = require("fs");
+
+let number_of_videos = 10;
+let comments = [];
+
+for (let i = 0 ; i < number_of_videos ; i += 1)
+{
+	comments.push ([]);
+}
+
+io.on ('connection', function(client){ 
+		console.log ("New connection " + client.id);
+
+client.on ('new_comment', function (data) {
+	console.log ('recieved ' + data);
+	let comment = data;
+	if (comment.id)
+	{
+		comments [comment.id].push ({author: comment.author, text: comment.text});
+	}
+	else
+	{
+		console.log ("Wrong query: " + data);
+	}
+});
 });
 
 app.get ('/', function (req, res) {
 	let answer_html = `
 	<html>
 		<body>
-		        <script src='/socket.io/socket.io.min.js'></script>
+		        <script src='/node_modules/socket.io-client/dist/socket.io.js'></script>
 				<script>
-					var socket = io();
-					socket.emit('new_comment', {id: 1, author: 'Alex Tsvetanov', text: 'TTSarq be tuk'});
+					var socket = io ();
+					socket.on('connect', function(){
+						alert (socket.id);
+						socket.emit('new_comment', {id: 1, author: 'Alex Tsvetanov', text: 'TTSarq be tuk'});
+						alert ("emitted");
+					});
 				</script>
 		</body>
 	</html>
@@ -53,7 +69,15 @@ app.get ('/', function (req, res) {
 	res.send (answer_html);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get ('/data', function (req, res) {
+	let id = parseInt (req.query.id);
+	console.log (req.query.id);
+	res.send (JSON.stringify (comments [id]));
 });
 
+app.get('*', function (req, res) {
+   res.sendFile( __dirname + "/node_modules/socket.io-client/dist/socket.io.js");
+})
+
+server.listen(3000);
+console.log('"Comment Service" listening on port 3000!');
